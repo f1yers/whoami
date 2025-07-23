0f9e21596409e9c84b7352d2b570cfc5a2df6a36
Nathan McGinnis <mcginnis.nathan@gmail.com>
2025-07-23 19:08:41 -0400

    moved middleware to src/

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request: Request) {
+  console.log("middleware is running");
+
   const url = new URL(request.url);
 
   if (url.hostname === 'nathanmcginnis.com') {
-    url.hostname = 'nathanmcginnis.dev';
+    url.hostname = 'https://nathanmcginnis.dev';
     return NextResponse.redirect(url, 301);
   }
 
