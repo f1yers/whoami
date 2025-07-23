cbea0601617d19913d0d87d4d0d6438c25783c94
Nathan McGinnis <mcginnis.nathan@gmail.com>
2025-07-23 18:02:37 -0400

    add middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  if (url.hostname === 'nathanmcginnis.com') {
+    url.hostname = 'nathanmcginnis.dev';
+    return NextResponse.redirect(url, 301);
+  }
+
+  return NextResponse.next();
+}
